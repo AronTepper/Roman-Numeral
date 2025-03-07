d1866f9d427f112c291ddfb07045a2d2d35a2feb
Aron Tepper <aron.tepper@gmail.com>
2025-03-07 14:56:05 +0100

    slice works

--- roman/roman.go
+++ roman/roman.go
@@ -2,22 +2,27 @@ package roman
 
 import "fmt"
 
+type pivot struct {
+	value   int
+	numeral string
+}
+
 func IntToRoman(number int) (string, error) {
-	var piv = map[int]string{
-		2000: "MM",
-		1000: "M",
-		900:  "CM",
-		500:  "D",
-		400:  "CD",
-		100:  "C",
-		90:   "XC",
-		50:   "L",
-		40:   "XL",
-		10:   "X",
-		9:    "IX",
-		5:    "V",
-		4:    "IV",
-		1:    "I",
+	var piv = []pivot{
+		{2000, "MM"},
+		{1000, "M"},
+		{900, "CM"},
+		{500, "D"},
+		{400, "CD"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
 	}
 
 	var res = ""
@@ -26,11 +31,11 @@ func IntToRoman(number int) (string, error) {
 		return "", fmt.Errorf("invalid number")
 	}
 
-	for idx, val := range piv {
-		fmt.Println(idx, val)
-		for number >= idx {
-			res += val
-			number -= idx
+	for _, val := range piv {
+		fmt.Println(val)
+		for number >= val.value {
+			res += val.numeral
+			number -= val.value
 		}
 	}
 
