2f41e8065038630c51f9de89c3b6e418fc878ec3
Aron Tepper <aron.tepper@gmail.com>
2025-03-07 16:22:18 +0100

    better :)

--- roman/roman.go
+++ roman/roman.go
@@ -1,6 +1,9 @@
 package roman
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type pivot struct {
 	value   int
@@ -8,8 +11,40 @@ type pivot struct {
 }
 
 func IntToRoman(number int) (string, error) {
+	if number < 0 {
+		return "", fmt.Errorf("invalid number")
+	}
+
+	thousands := number / 1000
+	hundreds := (number % 1000) / 100
+	tens := (number % 100) / 10
+	ones := number % 10
+
+	res := ""
+	res += strings.Repeat("M", thousands)
+	res += checkFunkyRomans(hundreds, "C", "D", "M")
+	res += checkFunkyRomans(tens, "X", "L", "C")
+	res += checkFunkyRomans(ones, "I", "V", "X")
+
+	return res, nil // fmt.Errorf("not implemented")
+}
+
+func checkFunkyRomans(number int, one, five, ten string) string {
+	if number < 4 {
+		return strings.Repeat(one, number)
+	}
+	if number == 4 {
+		return one + five
+	}
+	if number >= 5 && number < 9 {
+		return five + strings.Repeat(one, number-5)
+	}
+	return one + ten
+}
+
+func IntToRomanOld(number int) (string, error) {
 	var piv = []pivot{
-		{2000, "MM"},
+		// {2000, "MM"},
 		{1000, "M"},
 		{900, "CM"},
 		{500, "D"},
